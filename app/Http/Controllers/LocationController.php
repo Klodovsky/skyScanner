8c59746785afa58304c8561c9805ed1f904b4764
Klodovskyy <55706752+Klodovskyy@users.noreply.github.com>
2021-12-27 01:32:58 -0800

    fixing ip bug

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -10,10 +10,29 @@ class LocationController extends Controller
     public function index(Request $request)
     {
 
-        $ipAddress = $request->ip();
+        $ipAddress = $this->getUserIpAddr();
+
         $static = '197.30.131.167';
         $location = GeoLocation::lookup($static,$responseFilter = 'geo');
-
+       
         return json_encode($location);
+    }
+    public function getUserIpAddr(){
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
     }
 }
